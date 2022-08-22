c0652902ec1936d66f8f5dc5022b715ac1d0fc91
Agustin <agusmtx4@hotmail.com>
2022-08-22 09:58:50 +0100

    Commit inicial

---
+++ hola.js
@@ -0,0 +1,22 @@
+let nombre="quico";
+
+let numero1 = 5;
+let numero2 = 8;
+
+document.write(nombre + numero1 + numero2);
+
+let operacion = prompt("Qué operación deseas realizar (1: suma, 2: resta, 3: multiplicar, 4:dividir");
+
+let num1 = prompt("num1: ");
+let num2 = prompt("num2: ");
+
+let resultado;
+
+if (operacion == 1) {
+    resultado = num1 + num2;
+
+}
+
+const result = (suma) => {
+    
+}
